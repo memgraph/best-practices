aef4777c96511e0644dc3bfc31a91c009ad52422
antejavor <javor.ante@gmail.com>
2024-03-05 00:45:14 +0100

    Inital cleanup

--- import/cypher/neo4jpy/concurrent_edge_import.py
+++ import/cypher/neo4jpy/concurrent_edge_import.py
@@ -4,34 +4,70 @@ from time import sleep
 import subprocess
 from pathlib import Path
 from neo4j import GraphDatabase
+from neo4j.exceptions import TransientError
+from neo4j.debug import watch
+import random
+import sys
+import logging
 
-def execute_singe_transaction_query(query, create_list, max_retries=100, initial_wait_time=0.200, backoff_factor=1.1):
+#session._config.retry_delay_multiplier = 2.0
+#session._config.retry_delay_jitter_factor=0.2
+#session._config.max_transaction_retry_time=2000.0
+#session.inital_retry_delay = 1.0
+#TODO(antejavor): Test retry strategy
+
+#this is not configurable
+def execute_autocommit_implicit_API(query, create_list):
     driver = GraphDatabase.driver("bolt://localhost:7687", auth=("", ""))
     time_start = time.time()
-    with driver.session() as session:
-        for attempt in range(max_retries):
-            try:
-                session.run(query, {"batch": create_list})
-                break
-            except Exception as e:
-                wait_time = initial_wait_time * (backoff_factor ** attempt)
-                print(f"Commit failed on attempt {attempt+1}. Retrying in {wait_time} seconds...")
-                time.sleep(wait_time)
-        else:
-            print("Max retries reached. Commit failed.")
-            driver.close()
-            return time.time() - time_start
+    with driver.session(max_transaction_retry_time=180.0, initial_retry_delay=0.2, retry_delay_jitter_factor=1.1) as session:
+        session.run(query, {"batch": create_list})
+         
+    driver.close()
     time_end = time.time()
     return time_end - time_start
 
+def execute_managed_transaction_API(query, create_list):
+    driver = GraphDatabase.driver("bolt://localhost:7687", auth=("", ""))
+    time_start = time.time()
+    with driver.session(max_transaction_retry_time=180.0, initial_retry_delay=0.2, retry_delay_multiplier=1.1) as session:
+        session.execute_write(lambda tx: tx.run(query, {"batch": create_list}))
+    driver.close()
+    time_end = time.time()
+    return time_end - time_start
+
+def execute_explicit_transaction_API(query, create_list, max_retries=100, initial_wait_time=0.200, backoff_factor=1.1):
+    session = GraphDatabase.driver("bolt://localhost:7687", auth=("", "")).session()
+    time_start = time.time()
+    for attempt in range(max_retries):
+        try:
+            with session.begin_transaction() as tx:
+                tx.run(query, {"batch": create_list})
+                tx.commit()
+                break
+        except TransientError as te:
+            jitter = 0.1 * backoff_factor * random.random() 
+            wait_time = initial_wait_time * (backoff_factor ** attempt) + jitter
+            print(f"Commit failed on attempt {attempt+1}. Retrying in {wait_time} seconds...")
+            time.sleep(wait_time)
+        except Exception as e:
+            print(f"Failed to execute transaction: {e}")
+            break;
+    session.close()
+    time_end = time.time()
+    return time_end - time_start
+
+
 def process_chunk(query, create_nodes):
     print("Processing chunk...")
-    edge_execution_time = execute_singe_transaction_query(query, create_nodes)
+    edge_execution_time = execute_autocommit_implicit_API(query, create_nodes)
     print("Edge execution time: ", edge_execution_time)
     return edge_execution_time
 
 def run():
 
+    
+    
     #TODO(antejavor): Parametrize the size and file name
     size = "small"
 
--- import/cypher/pymgclient/concurrent_node_import.py
+++ import/cypher/pymgclient/concurrent_node_import.py
@@ -1,26 +1,29 @@
 import multiprocessing
 import mgclient
 import time
+import sys
 from time import sleep
 from pathlib import Path
 
-def execute_singe_transaction_query(query, create_list):
-    conn = mgclient.connect(host='127.0.0.1', port=7687)
-    cursor = conn.cursor()
-    time_start = time.time()
-    cursor.execute(query, {"batch": create_list})
-    conn.commit()
-    time_end = time.time()
-    conn.close()
-    return time_end - time_start
+class Sizes:
+    OPTIONS = ["small", "medium", "large"]
 
-def process_chunk(query, create_nodes):
-    print("Processing chunk...")
-    node_execution_time = execute_singe_transaction_query(query, create_nodes)
-    print("Node execution time: ", node_execution_time)
-    return node_execution_time
+def process_chunk(query, create_list):
+    try: 
+        conn = mgclient.connect(host='127.0.0.1', port=7687)
+        cursor = conn.cursor()
+        cursor.execute(query, {"batch": create_list})
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print("Failed to execute chunk: ", e)
+        raise e
 
-def run():
+
+def run(size: str):
+
+    CHUNK_SIZE = 10000
+    FILE_PATH = ""
      
     conn = mgclient.connect(host='127.0.0.1', port=7687)
     sleep(1)
@@ -32,19 +35,20 @@ def run():
         return
     cursor = conn.cursor()
 
-    #TODO(antejavor): Parametrize the size and file name
-    size = "small"
-    p = Path(__file__).parents[3].joinpath(f"datasets/graph500/{size}/graph500-scale18-ef16_adj.edges")
-    FILE_PATH = str(p)
-    CHUNK_SIZE = 10000
-    TOTAL_TIME = 0
+    p = Path(__file__).parents[3].joinpath(f"datasets/graph500/{size}")
+    for file in Path(p).iterdir():
+        if file.name.endswith(".nodes"):
+            FILE_PATH = str(file)
+            break
+
+    
     conn.autocommit = True
     cursor.execute("DROP INDEX ON :Node(id)")
     cursor.execute("MATCH (n) DETACH DELETE n")
+
     sleep(1)
 
     cursor.execute("CREATE INDEX ON :Node(id)")
-
     conn.autocommit = False
 
     query = """
@@ -53,44 +57,41 @@ def run():
     CREATE (n:Node {id:node.id})
     """
 
+    chunks = []
     with open(FILE_PATH, "r") as file:
         iteration = 0
         create_nodes = []
-        nodes_set = set()
-        chunks = []
         while True:
             line = file.readline()
             if not line:
                 if len(create_nodes) > 0:
                     chunks.append(create_nodes)
+                    print("Adding last chunk ...", len(create_nodes)
                     create_nodes = []
                 break
             else:
                 iteration += 1
-
-                node_sink, node_source = line.strip().split()  
-                if node_source not in nodes_set:
-                    create_nodes.append({"id": int(node_source)})
-                    nodes_set.add(node_source)
-                if node_sink not in nodes_set:
-                    create_nodes.append({"id": int(node_sink)})
-                    nodes_set.add(node_sink)
-
+                node_id = line.strip()
+                create_nodes.append({"id": int(node_id)})
                 if len(create_nodes) == CHUNK_SIZE:
-                    print("Chunk size reached - adding to chunks ...", len(nodes_set))
+                    print("Chunk size reached - adding to chunks ...", len(create_nodes))
                     chunks.append(create_nodes)
                     create_nodes = []
-        print("Starting processing chunks...")
-        start = time.time()
-        with multiprocessing.Pool(10) as pool:
-            results = pool.starmap(process_chunk, [(query, chunk) for chunk in chunks])
-            TOTAL_TIME = sum(results)
-        end = time.time()
-        print("Processing chunks finished in ", end - start, " seconds")
 
 
+    print("Starting processing chunks...")
+    start = time.time()
+    with multiprocessing.Pool(10) as pool:
+        pool.starmap(process_chunk, [(query, chunk) for chunk in chunks])
+    end = time.time()
+    print("Processing chunks finished in ", end - start, " seconds")
 
-    print("Total execution time: ", TOTAL_TIME)
 
 if __name__ == "__main__":
-    run()
+    if len(sys.argv) != 2:
+        print("Usage: python concurrent_node_import.py <size>")
+        sys.exit(1)
+    else:
+        size = sys.argv[1]
+        print(f"Running with size: {size}")
+        run(size=size)
