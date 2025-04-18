a2cf3a3afe7c8f520363163d32ae8eb365897e56
Josip Mrden <josip.mrden@memgraph.io>
2025-04-18 18:29:29 +0200

    Add example for creating read only user

---
+++ user_management/creating_read_only_user/creating_read_only_user.py
@@ -0,0 +1,39 @@
+from gqlalchemy import Memgraph
+
+memgraph = Memgraph()
+
+memgraph.execute("CREATE (n:Node)")
+
+# First user that is created is always the admin user with all privileges
+# After this command, you're no longer able to log in without credentials
+memgraph.execute("CREATE USER admin IDENTIFIED BY 'test'")
+
+
+# Create a readonly role with read privileges only
+memgraph.execute("CREATE ROLE readonly")
+# Grant privilege to execute the MATCH clause
+memgraph.execute("GRANT MATCH TO readonly")
+# Grant visibility on all node labels
+memgraph.execute("GRANT READ ON LABELS * TO readonly")
+# Grant visibility on all edge types
+memgraph.execute("GRANT READ ON EDGE_TYPES * TO readonly")
+
+
+memgraph.execute("CREATE USER readonly_user IDENTIFIED BY 'test'")
+# Assign the readonly role to the new user
+memgraph.execute("SET ROLE FOR readonly_user TO readonly")
+
+
+# By default, user will be able to log into the default database 'memgraph'
+print(list(memgraph.execute_and_fetch("SHOW DATABASE PRIVILEGES FOR readonly_user")))
+
+
+readonly_connection = Memgraph(username="readonly_user", password="test")
+print(list(readonly_connection.execute_and_fetch("MATCH (n) RETURN n")))
+
+
+try:
+    # This will not pass as user does not have permission
+    readonly_connection.execute("CREATE (n)")
+except Exception as e:
+    print(str(e))
